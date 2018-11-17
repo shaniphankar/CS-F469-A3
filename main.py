9cab6572c22cd7657d186e49d935699e177cd5f2
shaniphankar <shaniphankar@gmail.com>
2018-11-17 15:53:44 +0530

    Basics Laid Down

---
+++ main.py
@@ -0,0 +1,22 @@
+import nltk
+import os
+import json
+import pprint
+
+def main():
+	pp=pprint.PrettyPrinter(indent=4)	
+	f=open("data1.json",'r')
+	corpus=json.load(f)
+	f.close()
+	# pp.pprint(corpus)
+	data={}
+	data['en']=[]
+	data['fr']=[]
+	for dict in corpus:
+		data['en'].append(nltk.word_tokenize(dict['en']))
+		data['fr'].append(nltk.word_tokenize(dict['fr']))
+		# pp.pprint(nltk.word_tokenize(dict['en']))
+		# pp.pprint(nltk.word_tokenize(dict['fr'])) 
+	
+if __name__ == '__main__':
+	main()
