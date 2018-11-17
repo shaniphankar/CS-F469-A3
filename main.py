eaeab20c9d893a2574329f68b245e09519f513b9
Aditya Addepalli <dyex719@gmail.com>
2018-11-17 19:05:09 +0530

    Indian Json

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import pprint
 
 def main():
-	pp=pprint.PrettyPrinter(indent=4)	
+	pp=pprint.PrettyPrinter(indent=4)
 	f=open("data1.json",'r')
 	corpus=json.load(f)
 	f.close()
@@ -16,7 +16,7 @@ def main():
 		data['en'].append(nltk.word_tokenize(dict['en']))
 		data['fr'].append(nltk.word_tokenize(dict['fr']))
 		# pp.pprint(nltk.word_tokenize(dict['en']))
-		# pp.pprint(nltk.word_tokenize(dict['fr'])) 
-	
+		# pp.pprint(nltk.word_tokenize(dict['fr']))
+
 if __name__ == '__main__':
 	main()
