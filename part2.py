7be9b096275cc9eda0ce7b549e5921579b61fca8
Tanmay Kulkarni <tanmaygk97@gmail.com>
2018-11-22 15:38:16 +0530

    Increased readability

--- part2.py
+++ part2.py
@@ -2,7 +2,17 @@ import json
 import itertools
 from nltk.translate import IBMModel1,IBMModel2,AlignedSent
 def get_data():
-
+'''
+	Obtains the source data based on the settings set within the configuration file identified by "langsettings.json". 
+	Important Constants:
+		"train-data" = The training data to be used within the model
+		"source" = The language which is to be translated
+		"target" = The language to which the translation must be made.
+	Outputs:
+		corpus = A list of Alignment Objects, which inturn contain tuples of the source language, the target language and the possible alignment
+		source_set = The set of all the words present within the source
+		target_set = The set of all the words present within the target
+'''
 	# store the corpus for each model
 	corpus=[]
 	# sets for each type of word
@@ -31,7 +41,9 @@ def get_data():
 	return corpus,source_set,target_set,settings
 
 def get_examples(settings):
-	'''Extract the examples of the data'''
+	'''
+	Extract the examples of the data
+	'''
 
 	# open the file with the training data
 	with open(settings['train-data']) as f:
@@ -40,21 +52,44 @@ def get_examples(settings):
 		return examples
 
 def use_IBM1(corpus,settings):
-
+'''
+	Gives back the result on a corpus containing Aligned Objects, on using IBM Model 1
+	Inputs:
+		corpus = A list of Alignment Objects, which inturn contain tuples of the source language, the target language and the possible alignment
+		settings =  The hardcoded options set within the "langsettings.json" file
+	Outputs:
+		ibm1 = An object containing the mapping for the foreign words and the tranlated words and the probabilities of each
+		corpus = The modified input, which has the alignments for each word.
+'''
 	# train the model
 	ibm1=IBMModel1(corpus,settings['iterations'])
 
 	return ibm1,corpus
 
 def use_IBM2(corpus,settings):
-	
+'''
+	Gives back the result on a corpus containing Aligned Objects, on using IBM Model 2
+	Inputs:
+		corpus = A list of Alignment Objects, which inturn contain tuples of the source language, the target language and the possible alignment
+		settings =  The hardcoded options set within the "langsettings.json" file
+	Outputs:
+		ibm2 = An object containing the mapping for the foreign words and the tranlated words and the probabilities of each
+		corpus = The modified input, which has the alignments for each word.
+'''
 	# train the model
 	ibm2=IBMModel2(corpus,settings['iterations'])
 
 	return ibm2,corpus
 
 def print_data(ibm,source_set,target_set,model_num):
-
+'''
+	Prints the data for each mapping of the source word and the destination word.
+	Input:
+		ibm = The object returned by the ibm model containing all the mapping probabilities
+		source_set = The set of words belonging to the source
+		target_set = The set of words belonging to the target
+		model_num = Tells the number of the IBM Model used
+'''
 	product=itertools.product(source_set,target_set)
 
 
