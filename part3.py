1de2512ffb4c0f16f69f8f49b5b87c68b64baa1c
Tanmay Kulkarni <tanmaygk97@gmail.com>
2018-11-22 22:36:29 +0530

    minor changes to the inline documentation

--- part3.py
+++ part3.py
@@ -18,14 +18,14 @@ if __name__=='__main__':
 	for i in range(len(examples)):
 		phrases = phrase_based.phrase_extraction(examples[i][settings['source']],examples[i][settings['target']],corpus[i].alignment)
 		for i in sorted(phrases):
-			t = (i[2],i[3])
+			t = (i[2],i[3]) # tuple containing the source and the translation pair
 			if t not in foreign_eng: # check for first occurence of phrase pair
 				foreign_eng[t] = 1
 			else: #If phrase pair has already been encountered, increase count
 				foreign_eng[t] += 1
-			if i[2] not in english: #Check for first occurence of foreign phrase
+			if i[2] not in english: #Check for first occurence of source phrase
 				english[i[2]] = 1
-			else: #If foreign phrase has already been encountered, increase count
+			else: #If source phrase has already been encountered, increase count
 				english[i[2]] += 1
 
 	# scores of the phrases is determined by the formula score(f,e) = count(f,e)/count(f)
