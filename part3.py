8ad26f338cb5d124ea915f09d9baf9fa175c1afc
shaniphankar <shaniphankar@users.noreply.github.com>
2018-11-22 20:27:08 +0530

    hanged the divide count back to English. Sorry Aditya.

--- part3.py
+++ part3.py
@@ -11,34 +11,28 @@ if __name__=='__main__':
 	examples=get_examples(settings)
 	ibm,corpus=use_IBM1(corpus,settings)
 
-	# for i in range(len(examples)):
-	# 	phrases = phrase_based.phrase_extraction(examples[i][settings['source']],examples[i][settings['target']],corpus[i].alignment)
-	# 	for i in sorted(phrases):
-	# 		print(i)
-	# 	print("\n")
-
 	# foreign_eng contains the pairs of foreign and english words and their corresponding count
 	# eng contains the english words and their corresponding count
 	foreign_eng = {}
-	foreign = {}
+	english = {}
 	for i in range(len(examples)):
 		phrases = phrase_based.phrase_extraction(examples[i][settings['source']],examples[i][settings['target']],corpus[i].alignment)
 		for i in sorted(phrases):
 			t = (i[2],i[3])
-			# print(t)
-			if t not in foreign_eng: foreign_eng[t] = 1
-			else: foreign_eng[t] += 1
-			if i[3] not in foreign: foreign[i[3]] = 1
-			else: foreign[i[3]] += 1
-
-	# print(foreign_eng)
-	# print(eng)
+			if t not in foreign_eng: # check for first occurence of phrase pair
+				foreign_eng[t] = 1
+			else: #If phrase pair has already been encountered, increase count
+				foreign_eng[t] += 1
+			if i[2] not in english: #Check for first occurence of foreign phrase
+				english[i[2]] = 1
+			else: #If foreign phrase has already been encountered, increase count
+				english[i[2]] += 1
 
 	# scores of the phrases is determined by the formula score(f,e) = count(f,e)/count(f)
 	scores = {}
 
 	for phrase in foreign_eng:
-		scores[phrase] = foreign_eng[phrase]/foreign[phrase[1]]
+		scores[phrase] = foreign_eng[phrase]/english[phrase[0]]
 
 	scores = sorted(scores.items(),key=lambda x:x[1],reverse=True)
 	pp = pprint.PrettyPrinter(4)
